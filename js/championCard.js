c09c99e87b740119b05ffefc975f4c82de656170
Rafael Baldasso Audibert <32079912+rafaeelaudibert@users.noreply.github.com>
2018-02-21 23:35:42 -0300

    :lipstick: Improving tooltips code, and adding them

--- js/championCard.js
+++ js/championCard.js
@@ -1,10 +1,12 @@
 class championCard {
-  constructor(person){
+  constructor(person, color){
+    this.cardColor = color,
     this.championImage = "http://ddragon.leagueoflegends.com/cdn/${currentPatch}/img/champion/{person.championName}.png",
     this.championName = person.championName,
     this.championMasteryImage = person.championMasteryLevel,
     this.summonerName = person.summonerName,
-    this.summonerElo = "${person.summoner.liga.elo} ${person.summoner.liga.tier}",
+    this.summonerElo = "${person.liga.elo} ${person.liga.tier}",
+    this.pdl = person.liga.pdl,
     this.eloImage = "", // TODO: Create function responsible for setting the eloImage path
     this.runes = person.runes, // TODO: Create class responsible for setting the runes object (Will be set when requesting the runes)
     this.championWinrate = person.championWR !== 'First Match' ? '${person.championWR}%' :      person.championWR,
--- js/templates/todo.js
+++
@@ -1,8 +0,0 @@
-/**
- * TEMPLATES TO BE DONE
- */
-
-// TODO: Improve template of the cards when Tooltips are included
-// TODO: Template: Top Information - matchInformation
-// TODO: Template: Sidebar
-// TODO: Template: MODALS (Search, Profile, Settings)
