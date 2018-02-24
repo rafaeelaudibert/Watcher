9ad4c995ebc075b8a9d8d0deff3c3bc5d4cc6f7b
Rafael Baldasso Audibert <32079912+rafaeelaudibert@users.noreply.github.com>
2018-02-24 00:11:44 -0300

    :docs: Updating TODO lists

--- js/todo.js
+++ js/todo.js
@@ -3,6 +3,4 @@
  */
 
 // TODO: Set Translation Framework
-// TODO: Improve the region searched query parameter to use what is set in the Preferences
-// TODO: Deal with the preferences chosen by the user
-// TODO: When changing values in Settings, change close button to Apply, and only Apply if clicking the button, else doesn't change the actual values
+// TODO: Deal with the preferences chosen by the user (Switches)
