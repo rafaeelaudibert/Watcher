cbb10135a4ff4b34e2beb004fe11d6a30281c923
Rafael Baldasso Audibert <32079912+rafaeelaudibert@users.noreply.github.com>
2018-03-29 20:57:45 -0300

    :rocket: Creating the file which holds the global object Constants

---
+++ js/globalConstants.js
@@ -0,0 +1,119 @@
+// Constants for maps and modes
+const gameConstants = {
+  75: {
+    name: "6v6 Hexakill",
+    map: "Summoner's Rift"
+  },
+  76: {
+    name: "Ultra Rapid Fire (U.R.F.)",
+    map: "Summoner's Rift"
+  },
+  78: {
+    name: "One for All: Mirror Mode",
+    map: "Howling Abyss"
+  },
+  98: {
+    name: "6v6 Hexakill",
+    map: "Twisted Treeline"
+  },
+  100: {
+    name: "5v5 ARAM",
+    map: "Butcher's bridge"
+  },
+  310: {
+    name: "Nemesis",
+    map: "Summoner's Rift"
+  },
+  313: {
+    name: "Black Market Brawlers",
+    map: "Summoner's Rift"
+  },
+  317: {
+    name: "Definitely Not Dominion",
+    map: "Crystal Scar"
+  },
+  325: {
+    name: "All random",
+    map: "Summoner's Rift"
+  },
+  400: {
+    name: "5v5 Draft Pick",
+    map: "Summoner's Rift"
+  },
+  420: {
+    name: "5v5 Ranked Solo",
+    map: "Summoner's Rift"
+  },
+  430: {
+    name: "5v5 Blind Pick",
+    map: "Summoner's Rift"
+  },
+  440: {
+    name: "5v5 Ranked Flex",
+    map: "Summoner's Rift"
+  },
+  450: {
+    name: "5v5 ARAM",
+    map: "Howling Abyss"
+  },
+  460: {
+    name: "3v3 Blind Pick",
+    map: "Twisted Treeline"
+  },
+  470: {
+    name: "3v3 Ranked Flex",
+    map: "Twisted Treeline"
+  },
+  600: {
+    name: "Blood Hunt Assassin",
+    map: "Summoner's Rift"
+  },
+  610: {
+    name: "Dark Singularity",
+    map: "Cosmic Ruins"
+  },
+  900: {
+    name: "ARURF",
+    map: "Summoner's Rift"
+  },
+  910: {
+    name: "Ascension",
+    map: "Crystal Scar"
+  },
+  920: {
+    name: "Legend of The Poro King",
+    map: "Howling Abyss"
+  },
+  940: {
+    name: "Nexus Siege",
+    map: "Summoner's Rift"
+  },
+  950: {
+    name: "Doom Bots Voting",
+    map: "Summoner's Rift"
+  },
+  960: {
+    name: "Doom Bots",
+    map: "Summoner's Rift"
+  },
+  980: {
+    name: "Star Guardian Invasion: Normal",
+    map: "Valoran City Park"
+  },
+  990: {
+    name: "Star Guardian Invasion: Onslaught",
+    map: "Valoran City Park"
+  },
+  1000: {
+    name: "PROJECT: Hunters",
+    map: "Overcharge"
+  },
+  1010: {
+    name: "Snow ARURF",
+    map: "Summoner's Rift"
+  },
+  1020: {
+    name: "One for All",
+    map: "Summoner's Rift"
+  }
+}
